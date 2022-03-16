4016ff83d3bda4fd18941cf224f9dff9545f80ea
Spahr Michael <spahr.michael@protonmail.com>
2022-03-16 14:50:20 +0100

    add SafeMath.sol contract

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.6.6;
 
 // a library for performing overflow-safe math, courtesy of DappHub (https://github.com/dapphub/ds-math)
+//from uniswap v2
 
 library SafeMath {
     function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
@@ -13,5 +14,16 @@ library SafeMath {
 
     function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
         require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+    }
+
+    // from: https://github.com/ConsenSysMesh/openzeppelin-solidity/blob/master/contracts/math/SafeMath.sol
+    /**
+     * @dev Integer division of two numbers, truncating the quotient.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
     }
 }
